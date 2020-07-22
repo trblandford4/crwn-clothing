2df784e872a7a8561e4934f98d57caff46bdf480
Ray Blandford <ray@Rays-MBP.ngohome.priv>
2020-07-22 16:38:12 -0400

    Create new shop page
    - add shop page with routing
    - create collection preview and collecton item components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import HomePage from "./pages/homepage/HomePage.component.jsx";
-
 import "./App.css";
+
+import HomePage from "./pages/homepage/HomePage.component.jsx";
+import ShopPage from "./pages/shop/Shop.component.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
