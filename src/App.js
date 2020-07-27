2d5cb7969cd6d468000ea30879b52bc350a001b6
Ray Blandford <ray@Rays-MBP.ngohome.priv>
2020-07-27 14:25:41 -0400

    add signup component to sign in and sign up page add email/password auth to sign up and store in firestore db

--- src/App.js
+++ src/App.js
@@ -32,6 +32,8 @@ class App extends React.Component {
               ...snapshot.data(),
             },
           });
+
+          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
--- src/pages/sign-in-and-sign-up/SignInAndSignUp.component.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.component.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./SignInAndSignUp.styles.scss";
 import SignIn from "../../components/sign-in/SignIn.component";
+import SignUp from "../../components/sign-up/SignUp.component";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
